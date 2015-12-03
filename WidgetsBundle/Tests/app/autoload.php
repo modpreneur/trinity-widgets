cd34e873184abcfa84cd2f94cdbcb9a33ffec94b
Martin Kolek <prodejledu@gmail.com>
2015-12-03 15:13:56 +0100

    coment Tests app

--- WidgetsBundle/Tests/app/autoload.php
+++ WidgetsBundle/Tests/app/autoload.php
@@ -1,9 +1,9 @@
 <?php
-
-$loader = require __DIR__.'/../../../vendor/autoload.php';
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-require __DIR__.'/AppKernel.php';
+//
+//$loader = require __DIR__.'/../../../vendor/autoload.php';
+//use Doctrine\Common\Annotations\AnnotationRegistry;
+//
+//
+//AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+//
+//require __DIR__.'/AppKernel.php';
