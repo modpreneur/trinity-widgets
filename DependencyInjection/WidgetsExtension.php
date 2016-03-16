c57c65b94ecaac10d70bad16322c04c1ef150f4b
Tomáš Jančar <jan02@modpreneur.com>
2016-03-16 09:24:42 +0100

    test 5 :D

--- DependencyInjection/WidgetsExtension.php
+++ DependencyInjection/WidgetsExtension.php
@@ -17,7 +17,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class TrinityWidgetsExtension extends Extension
+class WidgetsExtension extends Extension
 {
     /**
      * {@inheritdoc}
